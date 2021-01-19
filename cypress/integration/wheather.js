9cc6041ad99aa3d6dd6df5cf73295f45001a41cc
Anne Caroline <carolsprak@gmail.com>
2021-01-19 01:29:41 -0300

    projeto inicial

---
+++ cypress/integration/wheather.js
@@ -0,0 +1,24 @@
+describe('Wheather API', () => {
+  it('should return status 200', () => {
+    const wheather = cy.request('GET', 'http://localhost:3082/api/v1/weather/Aracaju')
+    
+    wheather.its('status')
+      .should('be.eql', 200)
+    
+  })
+
+  it('should return content-type Json', () => {
+    const wheather = cy.request('GET', 'http://localhost:3082/api/v1/weather/Aracaju')
+
+    wheather.its('headers').its('content-type').should('include', 'application/json')
+  })
+
+  const getItems = () =>
+  cy.request('GET', 'http://localhost:3082/api/v1/weather/Aracaju')
+    .its('body')
+
+  it('returns name Aracaju', () => {
+      getItems()
+      .should('include', { name: 'Aracaju' });
+    })
+})
